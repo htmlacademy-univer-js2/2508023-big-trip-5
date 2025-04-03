a35e8b9580626c37eb7fc778cfec13e2d3a7c0e9
Королева Дарья <darakoroleva599@gmail.com>
2025-04-03 17:45:03 +0500

    Рефакторинг

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,12 +1,10 @@
 import { filter } from '../utils/filter.js';
 
-function generateFilter(points) {
-  return Object.entries(filter).map(
-    ([filterType, filterPoints]) => ({
-      type: filterType,
-      count: filterPoints(points).length,
-    }),
-  );
-}
+const generateFilter = (points) => Object.entries(filter).map(
+  ([filterType, filterPoints]) => ({
+    type: filterType,
+    count: filterPoints(points).length,
+  }),
+);
 
 export {generateFilter};
